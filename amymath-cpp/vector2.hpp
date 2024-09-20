2d69b5509cdbed299ee34778af021887726868be
Henry Wilder <henrythepony@gmail.com>
2024-09-20 01:18:34 -0400

    added vector2 functions

--- amymath-cpp/vector2.hpp
+++ amymath-cpp/vector2.hpp
@@ -5,6 +5,19 @@
 namespace amymath
 {
     template<std::floating_point T>
+    constexpr Vector2<T> operator+(const T &value, const Vector2<T> &v);
+
+    template<std::floating_point T>
+    constexpr Vector2<T> operator-(const T &value, const Vector2<T>& v);
+
+    template<std::floating_point T>
+    constexpr Vector2<T> operator*(const T &value, const Vector2<T>& v);
+
+    template<std::floating_point T>
+    constexpr Vector2<T> operator/(const T &value, const Vector2<T>& v);
+
+
+    template<std::floating_point T>
     struct Vector2
     {
         constexpr Vector2() :
@@ -33,123 +46,111 @@ namespace amymath
         T x = (T)0.0;
         T y = (T)0.0;
 
-        template<std::floating_point U>
-        constexpr Vector2 operator+(const Vector2<U> &other) const
+        constexpr Vector2 operator+(const Vector2 &other) const
         {
             Vector2 result = { x + other.x, y + other.y };
 
             return result;
         }
 
-        template<std::floating_point U>
-        constexpr Vector2 operator-(const Vector2<U> &other) const
+        constexpr Vector2 operator-(const Vector2 &other) const
         {
             Vector2 result = { x - other.x, y - other.y };
 
             return result;
         }
 
-        template<std::floating_point U>
-        constexpr Vector2 operator*(const Vector2<U> &other) const
+        constexpr Vector2 operator*(const Vector2 &other) const
         {
             Vector2 result = { x * other.x, y * other.y };
 
             return result;
         }
 
-        template<std::floating_point U>
-        constexpr Vector2 operator/(const Vector2<U> &other) const
+        constexpr Vector2 operator/(const Vector2 &other) const
         {
             Vector2 result = { x / other.x, y / other.y };
 
             return result;
         }
 
-        template<std::floating_point U>
-        constexpr Vector2 &operator+=(const Vector2<U> &other)
+        constexpr Vector2 &operator+=(const Vector2 &other)
         {
             return *this = { x + other.x, y + other.y };
         }
 
-        template<std::floating_point U>
-        constexpr Vector2 &operator-=(const Vector2<U> &other)
+        constexpr Vector2 &operator-=(const Vector2 &other)
         {
             return *this = { x - other.x, y - other.y };
         }
 
-        template<std::floating_point U>
-        constexpr Vector2 &operator*=(const Vector2<U> &other)
+        constexpr Vector2 &operator*=(const Vector2 &other)
         {
             return *this = { x * other.x, y * other.y };
         }
 
-        template<std::floating_point U>
-        constexpr Vector2 &operator/=(const Vector2<U> &other)
+        constexpr Vector2 &operator/=(const Vector2 &other)
         {
             return *this = { x / other.x, y / other.y };
         }
 
-        template<std::convertible_to<T> U>
-        constexpr Vector2 operator+(const U &value) const
+        constexpr Vector2 operator+(const T &value) const
         {
             Vector2 result = { x + value, y + value };
 
             return result;
         }
-
-        template<std::convertible_to<T> U>
-        constexpr Vector2 operator-(const U &value) const
+        
+        constexpr Vector2 operator-(const T &value) const
         {
             Vector2 result = { x - value, y - value };
 
             return result;
         }
-
-        template<std::convertible_to<T> U>
-        constexpr Vector2 operator*(const U &value) const
+        
+        constexpr Vector2 operator*(const T &value) const
         {
             Vector2 result = { x * value, y * value };
 
             return result;
         }
-
-        template<std::convertible_to<T> U>
-        constexpr Vector2 operator/(const U &value) const
+        
+        constexpr Vector2 operator/(const T &value) const
         {
             Vector2 result = { x / value, y / value };
 
             return result;
         }
-
-        template<std::convertible_to<T> U>
-        constexpr Vector2 &operator+=(const U &value)
+        
+        constexpr Vector2 &operator+=(const T &value)
         {
             return *this = { x + value, y + value };
         }
-
-        template<std::convertible_to<T> U>
-        constexpr Vector2 &operator-=(const U &value)
+        
+        constexpr Vector2 &operator-=(const T &value)
         {
             return *this = { x - value, y - value };
         }
-
-        template<std::convertible_to<T> U>
-        constexpr Vector2 &operator*=(const U &value)
+        
+        constexpr Vector2 &operator*=(const T &value)
         {
             return *this = { x * value, y * value };
         }
 
-        template<std::convertible_to<T> U>
-        constexpr Vector2 &operator/=(const U &value)
+        constexpr Vector2 &operator/=(const T &value)
         {
-            return *this = { x / value, y / value };
+            T reciprocal = (T)1.0 / value;
+            return *this = { x * reciprocal, y * reciprocal };
         }
 
         constexpr auto operator<=>(const Vector2 &other) const = default;
 
         constexpr Vector2 Abs() const
         {
-            Vector2 result = { std::abs(x), std::abs(y) };
+            Vector2 result = {
+                x < 0 ? -x : x,
+                y < 0 ? -y : y,
+            };
 
             return result;
         }
@@ -161,18 +162,90 @@ namespace amymath
             return result;
         }
 
-        template<numeric U, numeric V>
-        U AngleTo(const Vector2<V> &to) const
+        T AngleTo(const Vector2 &to) const
         {
-            // todo
+            T result = Angle() - to.Angle();
+
+            return result;
+        }
+        
+        T AngleToPoint(const Vector2 &to) const
+        {
+            T result = (to - *this).Angle();
+
+            return result;
         }
 
-        template<numeric U>
-        float AngleToPoint(const Vector2<U> &to) const
+        constexpr T Aspect() const
         {
-            return (to - *this).Angle();
+            return x / y;
+        }
+
+        static constexpr Vector2 Bezier2ndDerivative(const Vector2& p0, const Vector2& p1, const Vector2& p2, const Vector2& p3, T t)
+        {
+            T u = 1 - t;
+            return
+                (6 * u) * (p2 - 2 * p1 + p0) +
+                (6 * t) * (p3 - 2 * p2 + p1);
+        }
+
+        static constexpr Vector2 BezierDerivative(const Vector2& p0, const Vector2& p1, const Vector2& p2, const Vector2& p3, T t)
+        {
+            T u = 1 - t;
+            return
+                (3 * u * u) * (p1 - p0) +
+                (6 * u * t) * (p2 - p1) +
+                (3 * t * t) * (p3 - p2);
+        }
+
+        static constexpr Vector2 BezierInterpolate(const Vector2& p0, const Vector2& p1, const Vector2& p2, const Vector2& p3, T t)
+        {
+            T u = 1 - t;
+            return
+                (    u * u * u) * p0 +
+                (3 * u * u * t) * p1 +
+                (3 * u * t * t) * p2 +
+                (    t * t * t) * p3;
+        }
+
+        // reflect
+        constexpr Vector2 bounce(const Vector2 &normal) const
+        {
+            static_assert(false, "todo");
         }
     };
+
+    template<std::floating_point T>
+    constexpr Vector2<T> operator+(const T &value, const Vector2<T>& v)
+    {
+        Vector2 result = { value + v.x, value + v.y };
+
+        return result;
+    }
+    
+    template<std::floating_point T>
+    constexpr Vector2<T> operator-(const T &value, const Vector2<T>& v)
+    {
+        Vector2 result = { value - v.x, value - v.y };
+
+        return result;
+    }
+    
+    template<std::floating_point T>
+    constexpr Vector2<T> operator*(const T &value, const Vector2<T>& v)
+    {
+        Vector2 result = { value * v.x, value * v.y };
+
+        return result;
+    }
+    
+    template<std::floating_point T>
+    constexpr Vector2<T> operator/(const T &value, const Vector2<T>& v)
+    {
+        Vector2 result = { value / v.x, value / v.y };
+
+        return result;
+    }
 }
     
 template<std::floating_point T>
